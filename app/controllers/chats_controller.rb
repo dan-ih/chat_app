ee9aedde539b77df596dc6a1edd2c68224aba87f
Dan White <daniel.white@introhive.com>
2025-02-11 15:08:29 -0400

    set port num based on url

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,6 @@
 class ChatsController < ApplicationController
   def show
+    @anycable_id = params[:anycable_id] || "1" # Default to "1" if not provided
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "chats#show"
+  get '(:anycable_id)', to: 'chats#show', as: :chat, constraints: { anycable_id: /\d+/ }
   get "/manifest.json", to: "manifests#show"
   mount ActionCable.server => '/cable'
 
